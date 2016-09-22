1658494385b10cb0ed23138d0aa5bb0fb4b2fe80
progforce <nesterova.victoriya@progforce.com>
2016-09-22 16:49:45 +0300

    bootstrap conn

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,64 +1,23 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        <!-- Optional theme -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        <!-- Latest compiled and minified JavaScript -->
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 20px;
-            }
-        </style>
-    </head>
-    <body>
-    <div class="container">
-        <div class="page-header">
-            <h1>{{ $article->title }}</h1>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <ul class="list-group">
-                    <li class="list-group-item">{{ $article->name }}</li>
-                    <li class="list-group-item">{{ $article->body }}</li>
-                    <li class="list-group-item">{{ $article->author }}</li>
-                    <li class="list-group-item">1 -  {{ $article->created_at }}</li>
-                    <li class="list-group-item">2 -  {{ $article->updated_at }}</li>
-                </ul>
-            </div>
+@extends('welcome')
+@section('content')
+    <div class="page-header">
+        <h1>{{ $article->title }}</h1>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+            <ul class="list-group">
+                <li class="list-group-item">{{ $article->name }}</li>
+                <li class="list-group-item">{{ $article->body }}</li>
+                <li class="list-group-item">{{ $article->author }}</li>
+                <li class="list-group-item">1 -  {{ $article->created_at }}</li>
+                <li class="list-group-item">2 -  {{ $article->updated_at }}</li>
+            </ul>
         </div>
     </div>
-    
-    </body>
-</html>
+@endsection
+
+
+   
 
 
 
